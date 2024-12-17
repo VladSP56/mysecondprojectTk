07f37853be69c8b0015419c73f572167cd457ed6
VladSP56 <parkvls@gmail.com>
2024-12-17 23:37:26 +0300

    Update button.py

--- button.py
+++ button.py
@@ -1,8 +1,1 @@
-from tkinter import Tk, Button
 
-root = Tk()
-button1 = Button(root, text = "button 1")
-button2 = Button(root, text = "button 2")
-button1.grid(row = 0, column = 2)
-button2.grid(row = 1, column = 0)
-root.mainloop()
